93f3ecb0baf88d9087259240f5dcda64508f47f4
Xmausix <barbara.szymanska469@gmail.com>
2024-12-09 19:05:56 +0100

    stroke

---
+++ src/component/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import InfiniteGrid from "./components/main/InfinityGrid.vue";
+
+</script>
+
+<template>
+  <infinite-grid />
+</template>
+
+<style scoped>
+html,
+body {
+  height: 100%;
+  margin: 0;
+}
+
+</style>
